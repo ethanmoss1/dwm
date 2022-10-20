60ab9fac9c637c06855b8bac8bad53ce123a72eb
ethanmoss1 <cywinskimoss@gmail.com>
2022-10-20 14:36:23 +0100

    Cleaned up for addition of polybar, plus gitignore for build files

--- config.h
+++ config.h
@@ -7,34 +7,31 @@
 /* appearance */
 static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 10;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
+static const int showbar            = 0;       /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const int user_bh            = 36;       /* >0 sets bar height */
 static const int vertpad            = 10;       /* vertical padding of bar */
 static const int sidepad            = 10;       /* horizontal padding of bar */
 static const int gappx              = 10;       /* padding of windows */
-static const char *fonts[]          = { "RobotoMono Nerd Font:size=12" };
+static const char *fonts[]          = { "Roboto Mono Nerd Font:size=12" };
 
 /* Tag names */
-//static const char *tags[] = { " ﮟ ", "  ", "  ", " 4 ", " 5 ", " 6 ", " 7 ", " 8 ", " ﱘ " };
 static const char *tags[] = { " ﮟ ", "  ", "  ", " ﱘ " };
-
 static const Rule rules[] = {
 	/* class        instance    title    tags mask isfloating  Centre     monitor */
-	{ "Alacritty",  NULL,       NULL,    0,        0,     	   1,		  -1 },
+	{ "Alacritty",  NULL,       NULL,    0,        0,     	   1,	      -1 },
 	{ "Gimp",       NULL,       NULL,    0,        1,          0,         -1 },
 	{ "Firefox",    NULL,       NULL,    1 << 8,   0,          0,         -1 },
 };
 
 /* layout(s) */
-static const float mfact     = 0.6; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.67; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
-
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "靖",      tile },    /* first entry is default */
+	{ "靖",       tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 };
@@ -42,14 +39,14 @@ static const Layout layouts[] = {
 /* key definitions */
 #define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
-    { MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
-	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+  { MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
+  { MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
+  { MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
+  { MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
-#define STATUSBAR "dwmblocks"
+#define STATUSBAR "polybar" // dwmblocks, polybar, ect
 
 /* General Commands */
 static char dmenumon[2]             = "0"; /* component of dmenucmd, manipulated in spawn() */
@@ -66,10 +63,10 @@ static const char *cmdprv[]     = { "playerctl", "-a", "previous", NULL };
 static Key keys[] = {
 
 /*
- 
-    ORIGINAL KEYSBINDINGS - for reference when/if i want to use them 
 
-	 modifier                     key        function        argument 
+    ORIGINAL KEYSBINDINGS - for reference when/if i want to use them
+
+	 modifier                     key        function        argument
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -97,7 +94,7 @@ static Key keys[] = {
 */
 	/* modifier                     key                         function        argument */
 	{ MODKEY,                       XK_n,                       spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_t,                       spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_Return,                  spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_f,                       spawn,          {.v = firefoxcmd } },
 	{ MODKEY,                       XK_e,                       spawn,          {.v = emacscmd} },
 	{ MODKEY,                       XK_s,                       spawn,          {.v = screenshotcmd} },
@@ -113,14 +110,14 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                                       7)
 	TAGKEYS(                        XK_9,                                       8)
 	{ MODKEY|ShiftMask,             XK_q,                       quit,           {0} },
-    { 0,                            XF86XK_AudioRaiseVolume,    spawn,          SHCMD("amixer set Master 5%+; pkill -RTMIN+11 dwmblocks") },
-    { 0,                            XF86XK_AudioLowerVolume,    spawn,          SHCMD("amixer set Master 5%-; pkill -RTMIN+11 dwmblocks") },
-    { 0,                            XF86XK_AudioMute,           spawn,          SHCMD("amixer set Master togglemute; pkill -RTMIN+11 dwmblocks") },
-    { 0,                            XF86XK_AudioPlay,           spawn,          {.v = cmdplay } },
-    { 0,                            XF86XK_AudioPrev,           spawn,          {.v = cmdprv } },
-    { 0,                            XF86XK_AudioNext,           spawn,          {.v = cmdnxt } },
-    { 0,                            XF86XK_MonBrightnessUp,     spawn,          SHCMD("xbacklight -time 0 -inc 10; pkill -RTMIN+12 dwmblocks") },
-    { 0,                            XF86XK_MonBrightnessDown,   spawn,          SHCMD("xbacklight -time 0 -dec 10; pkill -RTMIN+12 dwmblocks") },
+	{ 0,                            XF86XK_MonBrightnessUp,     spawn,          SHCMD("xbacklight -time 0 -inc 10; pkill -RTMIN+12 dwmblocks") },
+	{ 0,                            XF86XK_MonBrightnessDown,   spawn,          SHCMD("xbacklight -time 0 -dec 10; pkill -RTMIN+12 dwmblocks") },
+	{ 0,                            XF86XK_AudioRaiseVolume,    spawn,          SHCMD("amixer set Master 5%+; pkill -RTMIN+11 dwmblocks") },
+	{ 0,                            XF86XK_AudioMute,           spawn,          SHCMD("amixer set Master togglemute; pkill -RTMIN+11 dwmblocks") },
+	{ 0,                            XF86XK_AudioLowerVolume,    spawn,          SHCMD("amixer set Master 5%-; pkill -RTMIN+11 dwmblocks") },
+	{ 0,                            XF86XK_AudioPrev,           spawn,          {.v = cmdprv } },
+	{ 0,                            XF86XK_AudioPlay,           spawn,          {.v = cmdplay } },
+	{ 0,                            XF86XK_AudioNext,           spawn,          {.v = cmdnxt } },
 };
 
 /* button definitions */
@@ -141,5 +138,5 @@ static Button buttons[] = {
 	{ ClkTagBar,            0,                  Button1,        view,           {0} },
 	{ ClkTagBar,            0,                  Button3,        toggleview,     {0} },
 	{ ClkTagBar,            MODKEY,             Button1,        tag,            {0} },
-	{ ClkTagBar,            MODKEY,             Button3,        toggletag,      {0} }, 
+	{ ClkTagBar,            MODKEY,             Button3,        toggletag,      {0} },
 };
